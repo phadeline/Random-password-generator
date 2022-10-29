0e62d169de55cff29cd39a03e376ff2f696d0079
phadeline <112015433+phadeline@users.noreply.github.com>
2022-10-29 18:31:49 -0400

    for loop has different possible password combos

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@
 let specialcharchoices = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 let numbers = "0123456789";
 let letters = "abcdefghijklmnopqrstuvwxyz";
-let lowercase = letters.toUpperCase();
+let uppercase = letters.toUpperCase();
 
 // These are the prompts that the user will receive their computer after they click generate password.
 function generatePassword() {
@@ -13,7 +13,10 @@ function generatePassword() {
 
   if (length < 8 || length > 128 || isNaN(length)) {
     alert("Chosen number must be between 8 to 128.");
+    return "";
   } else {
+    // Confirm returns a boolean value. Variables are created for the Confirm method in order to hold the value
+    // for use in the following for loop.
     let confirmspecial = confirm(
       "do you want special characters in your password?"
     );
@@ -23,13 +26,46 @@ function generatePassword() {
     let confirmlower = confirm(
       "do you want lower case letters in your password?"
     );
-    let confirmnuber = confirm("do you want numbers in your password?");
-    return myPassword();
+    let confirmnumber = confirm("do you want numbers in your password?");
+
+    let randomPassword = ""; // this variable is declared outside the for loop because if declared inside the loop
+    // it would not be accessible outside the for loop.
+
+    for (let i = 0; i < length; i++) {
+      let realPassword = "";
+      // the if statements below are created for each of the choices the user can choose from. Whatever choice is chosen will be concatenated
+      // to form the variable "realPassword".
+      if (confirmspecial) {
+        realPassword += specialcharchoices;
+      }
+
+      if (confirmnumber) {
+        realPassword += numbers;
+      }
+
+      if (confirmlower) {
+        realPassword += letters;
+      }
+
+      if (confirmupper) {
+        realPassword += uppercase;
+      }
+      if (
+        // This if statement was added in case the user chose "cancel" for each choice. It will return "".
+        confirmupper == false &&
+        confirmlower == false &&
+        confirmspecial == false &&
+        confirmnumber == false
+      ) {
+        alert("You must choose at least one choice.");
+        return "";
+      }
+      let randomnumber = Math.floor(Math.random() * realPassword.length);
+      randomPassword = randomPassword + realPassword[randomnumber];
+    }
+    return randomPassword;
   }
 }
-
-// This function contains all the possible combinations that the user can choose from.
-function myPassword() {}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -37,6 +73,11 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  if (password == "") {
+    // This was added in case the user chose "cancel" for each choice.
+    return;
+  }
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Password Generator Starter Code
+
+## Description
+
+The purpose of this project was to make a website that automatically generates a random password based on the user's
+chosen password length and choice of characters (numbers, lowercase letters and uppercase letters). First, the use will be asked to
+choose how long (between 8 - 128 charcaters) they want their password to be. Then they user will be asked if they want numbers, lowercase letters and uppercase letters in their password. Afterwards, a random password, based on the user's choices is created.
+
+## Installation
+
+N/A
+
+## Usage
+
+Users can generate a random password by clicking the "Generate Password" button. Afterwards, the user will be asked a series of
+questions and later a password is generated as explained in the Description section.
+
+## Credits
+
+The starter code was provided by Xander Rasptine, refer to Github profile https://github.com/Xandromus.
+
+## License
+
+Refer to the license in the repo.
+
+## Link to Application
